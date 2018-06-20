97c83764a0f96a7fd41156258db6b7815f8fb17e
Lainix <linkelin8@gmail.com>
2018-06-20 13:56:52 -0500

    Reto agregar contactos

---
+++ js/app.js
@@ -0,0 +1,66 @@
+var count = 0;
+
+
+// funcion para guardar lo que recibe el input
+// function getContactData() {
+//   var name = $('#contact-name').val();
+//   var phone = $('#contact-phone').val();
+  
+//   // para que pinte lo ingresado en consola
+//   console.log(name, phone);
+//   // agrego el contenido
+//   addContacDate(name, phone);
+//   // para limpiar el input
+//   $('#contact-name').val('');
+//   $('#contact-phone').val('');
+// }
+
+function getContactData () {
+  var $name = $('#contact-name');
+  var $phone = $('#contact-phone');
+  addContacDate($name.val(), $phone.val());
+  $name.val('');
+  $phone.val('');
+//  $('#contact-name').val('');
+//   $('#contact-phone').val('');
+};
+
+// crear plantilla- contatenamos los elementos del template
+var template = '<div class="row">'+
+                  '<div class="col offset-s3 s6">'+
+                    '<div class="card">'+
+                      '<div class="card-content orange lighten-3">'+
+                        '<h3>__name__</h3>'+
+                        '<span>__phone__</span>'+
+                        '<a class="waves-effect waves-light btn right">'+
+                          '<i class="material-icons">delete</i>'+
+                        '</a>'+
+                      '</div>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>'
+
+//  funcion para agregar contacto con a la plantilla
+function addContacDate (name, phone){
+  // contador
+  count++;
+  // variable para guardar
+  var finalTemplate = "";
+  // remplazo
+  finalTemplate = template.replace("__name__", name).replace("__phone__", phone);
+  // los digo donde va aparecer
+  $('main').append(finalTemplate);
+  // para el contador, que sube en el h5
+  $('h5').html('Total contactos:' + count);
+  // para dar mensaje de listo
+  swal("Yei!", "contacto agregado!", "success");
+}
+
+
+
+// para que lea e inicialice todo
+$(document).ready(function(){
+    $('.modal').modal();
+    $('#add-contact').click(getContactData);
+  });
+      
